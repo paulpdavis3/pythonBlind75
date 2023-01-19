9ad723daeb8b322e160406fc74a593d42d2115d1
Robotify <robotify@Robotifys-MacBook-Air.local>
2023-01-19 22:22:11 +0000

    program1 boilerplate added

--- p1.py
+++ p1.py
@@ -1,1 +1,12 @@
-print("hello world")
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        return [nums, target]
+
+
+print(Solution().twoSum([2,7,11,15], 9))
